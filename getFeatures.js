f5f491e982ba6075a4cc7ce1f58f26ceed1ffccf
Marco Brack <m.brack@mygcs.de>
2018-05-13 21:45:11 +0200

    Use async for getting features

--- getFeatures.js
+++ getFeatures.js
@@ -7,10 +7,22 @@ const _       = require('lodash');
 
 let token = fs.readFileSync('./token', { encoding: 'utf-8' });
 let [, userID, playlistID] = process.argv[2].match(/^spotify:user:(.+):playlist:(.+)$/);
-
 let uri = `https://api.spotify.com/v1/users/${userID}/playlists/${playlistID}/tracks?market=DE&fields=next%2Citems(track(id))&limit=100&offset=0`;
+
+// getTracks(uri, gotTracks);
+
 let tracks = [];
-getTracks(uri, gotTracks);
+async.waterfall(
+  [
+    async.constant(uri),
+    getTracks,
+    getFeatures,
+    _.partial(fs.writeFile, `${userID}_${playlistID}.json`)
+  ],
+  error => {
+    if (error) throw error
+  }
+)
 
 
 
@@ -32,16 +44,39 @@ function getTracks(uri, callback) {
   });
 }
 
-function gotTracks(error, tracks) {
-  let chunks = _.chunk(tracks, 100);
-  async.map(chunks, getFeatures, gotFeatures);
+function getFeatures(tracks, callback) {
+  async.waterfall(
+    [
+      async.constant(tracks),
+      chunkTracks,
+      getFeaturesForChunks,
+      unchunkFeatures
+    ],
+    callback
+  )
+
+  function chunkTracks(tracks, callback) {
+    return callback(null, chunks = _.chunk(tracks, 100))
+  }
+
+  function getFeaturesForChunks(chunks, callback) {
+    return async.map(chunks, getFeaturesForChunk, callback);
+  }
+
+  function getFeaturesForChunk(chunk, callback) {
+    let ids = chunk.map(item => item.track.id).join('%2C');
+    let uri = `https://api.spotify.com/v1/audio-features?ids=${ids}`;
+    return getWithAuth(uri, callback);
+  }
+
+  function unchunkFeatures(chunkFeatures) {
+    let features = [].concat(...chunkFeatures.map(chunk => chunk.audio_features))
+    console.log(features)
+    fs.writeFileSync(`${userID}_${playlistID}.json`, JSON.stringify(features));
+  }
 }
 
-
-function getFeatures(chunk, callback) {
-  let ids = chunk.map(item => item.track.id).join('%2C');
-  let uri = `https://api.spotify.com/v1/audio-features?ids=${ids}`;
-
+function getWithAuth(uri, callback) {
   request.get(uri, {headers: {
     "Accept": "application/json",
     "Content-Type": "application/json",
@@ -49,12 +84,7 @@ function getFeatures(chunk, callback) {
   }}, (error, response, body) => {
     if (error) { console.error(error); return; }
     body = JSON.parse(body);
-    callback(null, body);
+    return callback(null, body);
   });
 }
 
-function gotFeatures(error, chunkFeatures) {
-  let features = [].concat(...chunkFeatures.map(chunk => chunk.audio_features))
-  console.log(features)
-  fs.writeFileSync(`${userID}_${playlistID}.json`, JSON.stringify(features));
-}
